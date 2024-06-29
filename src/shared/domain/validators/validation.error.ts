4c5ad3f336cee2a4815c168e408cb328a505c46a
genesluna <genesluna@gmail.com>
2024-06-29 00:47:44 +0000

    feat: add entity validation draft

---
+++ src/shared/domain/validators/validation.error.ts
@@ -0,0 +1,10 @@
+import { FieldsErrors } from "./validator-fields-interface";
+
+export class EntityValidationError extends Error {
+  constructor(public error: FieldsErrors, message = "Entity validation error") {
+    super(message);
+  }
+  count() {
+    return Object.keys(this.error).length;
+  }
+}
